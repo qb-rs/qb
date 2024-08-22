f40901ee15c09b4a789420017600bf5151cb19d6
Lucas Birkert <lucasbirkert@gmail.com>
2024-08-22 20:09:28 +0200

    Try to implement singleton instead (failed)

--- qb-mobile/rust/src/api/daemon.rs
+++ qb-mobile/rust/src/api/daemon.rs
@@ -1,14 +1,20 @@
-use std::path::PathBuf;
+use std::{
+    path::PathBuf,
+    sync::{LazyLock, Mutex},
+};
 
+use flutter_rust_bridge::frb;
 use qb_core::fs::wrapper::QBFSWrapper;
 use qb_daemon::{daemon::QBDaemon, master::QBMaster};
 
-pub struct DaemonWrapper {
-    daemon: QBDaemon,
-}
+// TODO: figure out how to make QBDaemon send.
+static DAEMON_SINGLETON: LazyLock<Mutex<Option<QBDaemon>>> = LazyLock::new(|| Mutex::new(None));
+
+#[frb(opaque)]
+pub struct DaemonWrapper {}
 
 impl DaemonWrapper {
-    pub async fn init(path: String) -> DaemonWrapper {
+    pub async fn init(path: String) {
         let path: PathBuf = path.into();
         let wrapper = QBFSWrapper::new(path);
 
@@ -16,6 +22,6 @@ impl DaemonWrapper {
         let mut daemon = QBDaemon::init(master, wrapper).await;
         daemon.autostart().await;
 
-        Self { daemon }
+        *DAEMON_SINGLETON.lock().unwrap() = Some(daemon);
     }
 }
